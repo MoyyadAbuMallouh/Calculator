bc1b9877e5f1d00283adc734bd86ce59ccfb8cf1
Mallouh Moyyad <abumallouhmoyyad1@gmail.com>
2024-06-25 06:01:17 +0300

    div feature

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static int mul()//emp2 should work here
     {return 0;}
 
-    public static int div()////emp2 should work here
-    {return 0;}
+    public static int div(int x ,int y)////emp2 should work here
+    {return x/y;}
 
 
     public static void main(String[] args)//tester should test code here!
