fd05dff0a2bfd43266a3dd2b58eece420d160767
Mallouh Moyyad <abumallouhmoyyad1@gmail.com>
2024-06-25 05:59:24 +0300

    fixing

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static int mul(int x,int y)//emp2 should work here
     {return (x*y);}
 
-    public static int div(int x,int y)////emp2 should work here
-    {return x/y;}
+    public static int div()////emp2 should work here
+    {return 0;}
 
 
     public static void main(String[] args)//tester should test code here!
