afa620b1aeb67c84c330e3f23a3868f7f7df1e10
Mallouh Moyyad <abumallouhmoyyad1@gmail.com>
2024-06-25 04:49:44 +0300

    subtraction feature

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static int sub()//emp1 should work here
     {return 0;}
 
-    public static int mul()//emp2 should work here
-    {return 0;}
+    public static int mul(int x,int y)//emp2 should work here
+    {return (x*y);}
 
     public static int div()////emp2 should work here
     {return 0;}
